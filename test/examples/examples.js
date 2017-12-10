5afe488b4b7088785d37b07400747e1af7c1aee1
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-11 00:11:39 +0100

    Add types to example tests

--- test/examples/examples.js
+++ test/examples/examples.js
@@ -1,28 +1,38 @@
 const test = require('ava')
 const { evaluateRaw } = require('../../src')
 
+const int = (value) => ({
+  type: 'int256',
+  value
+})
+
+const address = (value) => ({
+  type: 'address',
+  value
+})
+
 const cases = [
   // Bindings
   [{
     source: 'a is `a`, b is `b` and "c d" is `c d`',
-    bindings: { a: 1, b: 2, c: 3, d: 4 }
+    bindings: { a: int(1), b: int(2), c: int(3), d: int(4) }
   }, 'a is 1, b is 2 and "c d" is 3 4'],
 
   // Maths
   [{
     source: 'Will multiply `a` by 7 and return `a * 7`',
-    bindings: { a: 122 }
+    bindings: { a: int(122) }
   }, 'Will multiply 122 by 7 and return 854'],
 
   [{
    source: 'Basic arithmetic: `a` + `b` is `a + b`, - `c` that\'s `a + b - c`, quick mafs',
-   bindings: { a: 2, b: 2, c: 1 }
+   bindings: { a: int(2), b: int(2), c: int(1) }
   }, 'Basic arithmetic: 2 + 2 is 4, - 1 that\'s 3, quick mafs'],
 
   // External calls
   [{
     source: 'Allocate `amount token.symbol(): string`.',
-    bindings: { amount: 100, token: '0x960b236A07cf122663c4303350609A66A7B288C0' }
+    bindings: { amount: int(100), token: address('0x960b236A07cf122663c4303350609A66A7B288C0') }
   }, 'Allocate 100 ANT.'],
   [{
     source: 'Burns the `token.symbol(): string` balance of `person` (balance is `token.balanceOf(person: address): uint256`)',
