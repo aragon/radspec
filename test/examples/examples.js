67ccab66fd8132f1c98a6a57ab9ad6894998d707
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-10 22:17:44 +0100

    Add quick mafs test case

--- test/examples/examples.js
+++ test/examples/examples.js
@@ -5,7 +5,12 @@ const cases = [
   [{
     source: 'Will multiply `a` by 7 and return `a * 7`',
     bindings: { a: 122 }
-  }, 'Will multiply 122 by 7 and return 854']
+  }, 'Will multiply 122 by 7 and return 854'],
+
+  [{
+   source: 'Basic arithmetic: `a` + `b` is `a + b`, - `c` that\'s `a + b - c`, quick mafs',
+   bindings: { a: 2, b: 2, c: 1 }
+  }, 'Basic arithmetic: 2 + 2 is 4, - 1 that\'s 3, quick mafs']
 ]
 
 test('Examples', async (t) => {
