c9ce50da8b39ce76f327b650a6a978e893ce2b40
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-10 22:52:43 +0100

    Add test cases for external calls and bindings

--- test/examples/examples.js
+++ test/examples/examples.js
@@ -2,6 +2,13 @@ const test = require('ava')
 const { evaluateRaw } = require('../../src')
 
 const cases = [
+  // Bindings
+  [{
+    source: 'a is `a`, b is `b` and "c d" is `c d`',
+    bindings: { a: 1, b: 2, c: 3, d: 4 }
+  }, 'a is 1, b is 2 and "c d" is 3 4'],
+
+  // Maths
   [{
     source: 'Will multiply `a` by 7 and return `a * 7`',
     bindings: { a: 122 }
@@ -10,7 +17,13 @@ const cases = [
   [{
    source: 'Basic arithmetic: `a` + `b` is `a + b`, - `c` that\'s `a + b - c`, quick mafs',
    bindings: { a: 2, b: 2, c: 1 }
-  }, 'Basic arithmetic: 2 + 2 is 4, - 1 that\'s 3, quick mafs']
+  }, 'Basic arithmetic: 2 + 2 is 4, - 1 that\'s 3, quick mafs'],
+
+  // External calls
+  [{
+    source: 'Allocate `amount token.symbol(): string`.',
+    bindings: { amount: 100, token: '0x960b236A07cf122663c4303350609A66A7B288C0' }
+  }, 'Allocate 100 ANT.']
 ]
 
 test('Examples', async (t) => {
@@ -19,7 +32,7 @@ test('Examples', async (t) => {
     t.is(
       actual,
       expected,
-      `Expected "${input}" to evaluate to "${expected}", but evaluated to "${actual}"`
+      `Expected "${input.source}" to evaluate to "${expected}", but evaluated to "${actual}"`
     )
   }
 })
