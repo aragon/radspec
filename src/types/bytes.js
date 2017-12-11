46aaabfcbc97ddd6cf09f3b4ef27561a4c4aa518
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-11 01:54:49 +0100

    Support `byte` (alias for `bytes1`)

--- src/types/bytes.js
+++ src/types/bytes.js
@@ -2,6 +2,12 @@ module.exports = {
   isType (identifier) {
     const n = identifier.substr(5)
 
+    // `byte` is bytes1
+    if (!n && identifier === 'byte') {
+      identifier = 'bytes1'
+      n = 1
+    }
+
     return identifier.startsWith('bytes') &&
       n <= 32
   }
