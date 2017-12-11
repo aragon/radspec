2323ea681e249f2ab7e149ac0b01463f85c93db2
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-11 01:55:10 +0100

    Support `uint` (alias for `uint256`)

--- src/types/uint.js
+++ src/types/uint.js
@@ -1,6 +1,9 @@
 module.exports = {
   isType (identifier) {
-    const n = identifier.substr(4)
+    let n = identifier.substr(4)
+
+    // Default to uint256
+    if (!n) n = 256
 
     return identifier.startsWith('uint') &&
       (n % 8 === 0) &&
