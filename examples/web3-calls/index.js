5b3fc61f1ee5a5b51d8bef4f398a065fac8fc802
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-01 14:10:39 +0200

    Add examples for self and nested expressions

--- examples/web3-calls/index.js
+++ examples/web3-calls/index.js
@@ -1,24 +1,51 @@
 const radspec = require('../../src')
 
-const expression = 'Allocate `amount token.symbol(): string`.'
-const call = {
-  abi: [{
-    name: 'allocate',
-    constant: false,
-    type: 'function',
-    inputs: [{
-      name: 'token',
-      type: 'address'
-    }, {
-      name: 'amount',
-      type: 'uint256'
-    }],
-    outputs: []
-  }],
-  transaction: {
-    data: '0xb78b52df000000000000000000000000960b236a07cf122663c4303350609a66a7b288c00000000000000000000000000000000000000000000000000000000000000064'
+const expressions = [
+  {
+    expression: 'Deposit `_amount` to `(_token.controller(): address).sale(): address` `_token.symbol(): string`',
+    call: {
+      abi: [{
+        name: 'allocate',
+        constant: false,
+        type: 'function',
+        inputs: [{
+          name: '_token',
+          type: 'address'
+        }, {
+          name: '_amount',
+          type: 'uint256'
+        }],
+        outputs: []
+      }],
+      transaction: {
+        data: '0xb78b52df000000000000000000000000960b236a07cf122663c4303350609a66a7b288c00000000000000000000000000000000000000000000000000000000000000064'
+      }
+    }
+  },
+  {
+    expression: 'Send `_amount` ETH to `(_token.controller(): address).sale(): address` to buy `_token.symbol(): string`',
+    call: {
+      abi: [{
+        name: 'allocate',
+        constant: false,
+        type: 'function',
+        inputs: [{
+          name: '_token',
+          type: 'address'
+        }, {
+          name: '_amount',
+          type: 'uint256'
+        }],
+        outputs: []
+      }],
+      transaction: {
+        data: '0xb78b52df000000000000000000000000960b236a07cf122663c4303350609a66a7b288c00000000000000000000000000000000000000000000000000000000000000064'
+      }
+    }
   }
-}
+]
 
-radspec.evaluate(expression, call)
-  .then(console.log) // => "Allocate 100 ANT."
+expressions.forEach(({ expression, call }) => {
+  radspec.evaluate(expression, call)
+    .then(console.log) // => "Allocate 100 ANT."
+})
