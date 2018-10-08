4352ea7953f96203ca97a98360673ba6a9eb153b
Jorge Izquierdo <jorge@izqui.me>
2018-10-08 13:42:28 +0200

    Improve formatBN

--- src/helpers/lib/formatBN.js
+++ src/helpers/lib/formatBN.js
@@ -4,14 +4,18 @@ exports.tenPow = x => (
   (new BN(10, 10)).pow(new BN(x, 10))
 )
 
-exports.formatBN = (value, base, precision) => {
+exports.formatBN = (amount, base, precision) => {
   // Inspired by: https://github.com/ethjs/ethjs-unit/blob/35d870eae1c32c652da88837a71e252a63a83ebb/src/index.js#L83
   const baseLength = base.toString().length
 
-  let fraction = value.mod(base).toString()
+  const whole = amount.div(base).toString()
+  let fraction = amount.mod(base).toString()
   const zeros = '0'.repeat(Math.max(0, baseLength - fraction.length - 1))
-  fraction = `${zeros}${fraction}`
-  const whole = value.div(base).toString()
+  fraction = `${zeros}${fraction}`.replace(/0+$/, '').slice(0, precision)
 
-  return `${whole}${parseInt(fraction, 10) === 0 ? '' : `.${fraction.slice(0, precision)}`}`
+  if (fraction === '' || parseInt(fraction, 10) === 0) {
+    return whole
+  }
+
+  return `${whole}.${fraction}`
 }
