cfead8e8619cd65505bc22b1ca0dcc79c00783f5
Jorge Izquierdo <jorge@izqui.me>
2019-02-11 15:50:51 +0100

    Make known functions a JS file

--- src/data/knownFunctions.js
+++ src/data/knownFunctions.js
@@ -1,6 +1,7 @@
-{
+module.exports = {
     "setOwner(address)": "Set `$1` as the new owner",
     "setOwner(bytes32,address)": "Set `$2` as the new owner of the `$1` node",
     "transfer(address,address,uint256)": "Transfer `@tokenAmount($1, $3)` to `$2`",
-    "payday()": "Get owed Payroll allowance"
+    "payday()": "Get owed Payroll allowance",
+    "isForwarder()": "Very important action"
 }
