1a591eeb0a8a770d909a515eb3ee49e06a8bc157
Sebastian Siemssen <fubhy@fubhy.com>
2020-07-08 15:09:05 +0200

    feat: add melonprotocol known functions

---
+++ src/data/melonprotocol/knownFunctions.js
@@ -0,0 +1,18 @@
+export default {
+  'registerAsset(address,string,string,string,uint,uint[],bytes4[])': 'Register new asset `$3` (`$2`) at `$1` (reserve min: `$5`)',
+  'registerExchangeAdapter(address,address,bool,bytes4[])': 'Register new adapter `$1` for exchange `$2`',
+  'registerVersion(address,bytes32)': 'Register new version `$1`',
+  'setIncentive(uint)': 'Set incentive to `$1`',
+  'setPriceSource(address)': 'Set price source to `$1`',
+  'setMlnToken(address)': 'Set Melon token to `$1`',
+  'setNativeAsset(address)': 'Set native asset to `$1`',
+  'setEngine(address)': 'Set engine to `$1`',
+  'setMGM(address)': 'Set MGM to `$1`',
+  'setEthfinexWrapperRegistry(address)': 'Set Ethfinex wrapper registry to `$1`',
+  'removeAsset(address,uint)': 'Remove asset `$1`',
+  'removeExchangeAdapter(address,uint)': 'Remove exchange adapter `$1`',
+  'registerFees(address[])': 'Register fees `$1`',
+  'deregisterFees(address[])': 'De-register fees `$1`',
+  'setRegistry(address)': 'Set registry to `$1`',
+  'setAmguPrice(uint)': 'Set AMGU price to `$1`'
+}
