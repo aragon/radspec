01f573da2425bd19bf801afdac3629023894448a
Sebastian Siemssen <fubhy@fubhy.com>
2020-07-31 11:07:07 +0200

    feat: add melonprotocol known functions (#103)

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,11 @@
 import dclKnownFunctions from './decentraland/knownFunctions'
 import nucypherKnownFunctions from './nucypher/knownFunctions'
+import melonprotocolKnownFunctions from './melonprotocol/knownFunctions'
 import baseKnownFunctions from './knownFunctions'
 
 export const knownFunctions = {
   ...dclKnownFunctions,
   ...nucypherKnownFunctions,
+  ...melonprotocolKnownFunctions,
   ...baseKnownFunctions
 }
